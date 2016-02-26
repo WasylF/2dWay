5a42cb27fed759b6ce454f2cba13a4f94582df07
Vasyl Franchuk <WasylFranchuk@gmail.com>
2016-02-26 12:03:25 +0200

    init NetBeans project

---
+++ pilot-problem/src/wslf/Main.java
@@ -0,0 +1,16 @@
+package wslf;
+
+/**
+ *
+ * @author Wsl_F
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
