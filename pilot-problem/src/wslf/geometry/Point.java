18ad6a68fd13d1c1e98822e42222b4699611dd65
Vasyl Franchuk <WasylFranchuk@gmail.com>
2016-02-27 10:11:55 +0200

    add basics fields and methods

--- pilot-problem/src/wslf/geometry/Point.java
+++ pilot-problem/src/wslf/geometry/Point.java
@@ -5,5 +5,21 @@ package wslf.geometry;
  * @author Wsl_F
  */
 public class Point {
-    
+
+    double x;
+    double y;
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p) {
+        return Math.sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y));
+    }
 }
