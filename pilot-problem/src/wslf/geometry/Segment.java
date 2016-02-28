67bb34d88a4104a72b05b2b9d68a1b9e2053de09
Wsl_F <WasylFranchuk@gmail.com>
2016-02-28 19:04:57 +0200

    add isIntersect(Segment) and contains(Point) methods

--- pilot-problem/src/wslf/geometry/Segment.java
+++ pilot-problem/src/wslf/geometry/Segment.java
@@ -44,4 +44,51 @@ public class Segment {
         return a.distance(b);
     }
 
+    /**
+     * if this segment contains Point p return true else return false
+     *
+     * @param p Point
+     * @return
+     */
+    public boolean contains(Point p) {
+        if (a.equals(p) || b.equals(p)) {
+            return true;
+        }
+        Vector v1 = new Vector(a, p);
+        Vector v2 = new Vector(b, p);
+
+        return v1.isCollinear(v2) && !v1.isUnidirectional(v2);
+    }
+
+    /**
+     * if segments intersects return true else return false
+     *
+     * @param segm second segment
+     * @return
+     */
+    public boolean isIntersect(Segment segm) {
+        if (contains(segm.a) || contains(segm.b)
+                || segm.contains(a) || segm.contains(b)) {
+            return true;
+        }
+
+        Vector ab = new Vector(a, b);
+        Vector v1 = new Vector(a, segm.a);
+        Vector v2 = new Vector(a, segm.b);
+
+        if (ab.sgnMultiplyVectors(v1) == ab.sgnMultiplyVectors(v2)) {
+            return false; // if line AB doesn't intersect segment CD
+        }
+
+        ab = new Vector(segm.a, segm.b);
+        v1 = new Vector(segm.a, a);
+        v2 = new Vector(segm.a, b);
+
+        if (ab.sgnMultiplyVectors(v1) == ab.sgnMultiplyVectors(v2)) {
+            return false; // if line CD doesn't intersect segment AB
+        }
+
+        v1 = new Vector(a, b);
+        return !ab.isCollinear(v1);
+    }
 }
