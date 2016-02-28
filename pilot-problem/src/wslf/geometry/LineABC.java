e281853441cb3e25b594f59d1e4eabeceb7e6d89
Wsl_F <WasylFranchuk@gmail.com>
2016-02-28 21:58:31 +0200

    Create class LineABC and add basics constructors and methods

---
+++ pilot-problem/src/wslf/geometry/LineABC.java
@@ -0,0 +1,70 @@
+package wslf.geometry;
+
+import static java.lang.Math.*;
+import static wslf.geometry.Constants.*;
+import static wslf.geometry.Math.*;
+
+/**
+ * a*x + b*y + c = 0
+ *
+ * @author Wsl_F
+ */
+public class LineABC {
+
+    double a;
+    double b;
+    double c;
+
+    public LineABC(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public LineABC() {
+        this.a = 0;
+        this.b = 0;
+        this.c = 0;
+    }
+
+    public LineABC(Point A, Point B) {
+        if (A.equals(B)) {
+            throw new IllegalArgumentException("Points the same");
+        }
+
+        a = B.y - A.y;
+        b = A.x - B.x;
+        c = -determinant(A, B);
+    }
+
+    public LineABC(Segment segm) {
+        this(segm.a, segm.b);
+    }
+
+    public LineABC(Point p, Vector v) {
+        this(p, new Point(p.x + v.x, p.y + v.y));
+    }
+
+    @Override
+    public String toString() {
+        return "<" + a + " x  +  " + b + " y  +  " + c + ">";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != getClass()) {
+            return false;
+        }
+
+        return equals((LineABC) obj);
+    }
+
+    public boolean equals(LineABC line) {
+        return isDeterminantZero(a, b, line.a, line.b)
+                && isDeterminantZero(a, c, line.a, line.c)
+                && isDeterminantZero(b, c, line.b, line.c);
+    }
+}
