2f81584f2ecbc9ad3746b32bc5eb1268f7db3b6b
Wsl_F <WasylFranchuk@gmail.com>
2016-03-26 14:48:00 +0200

    add field and constructor

--- pilot-problem/src/wslf/algo/World.java
+++ pilot-problem/src/wslf/algo/World.java
@@ -1,9 +1,25 @@
 package wslf.algo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import wslf.geometry.*;
+
 /**
  *
  * @author Wsl_F
  */
 public class World {
-    
+
+    /**
+     * array that contains all barriers in the world
+     */
+    final Polygon[] barriers;
+
+    public World(Polygon[] barriers) {
+        this.barriers = barriers;
+    }
+
 }
