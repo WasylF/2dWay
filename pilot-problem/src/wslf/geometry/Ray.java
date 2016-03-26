1654c8d5393c650144b2d5d73444e7ff00e4070b
Wsl_F <WasylFranchuk@gmail.com>
2016-03-26 12:59:14 +0200

    add 'intersections' methods

--- pilot-problem/src/wslf/geometry/Ray.java
+++ pilot-problem/src/wslf/geometry/Ray.java
@@ -39,4 +39,51 @@ public class Ray {
 
         return equals((Ray) obj);
     }
+
+    /**
+     * does Ray contains point. Works with null point
+     *
+     * @param point point
+     * @return
+     */
+    public boolean contains(Point point) {
+        if (point == null) {
+            return false;
+        }
+        return v.getAngle(new Vector(this.p, point)) < Constants.EPS_ANGLE;
+    }
+
+    /**
+     * find intersection of Ray and segment. If doesn't exists - null
+     *
+     * @param sg segment
+     * @return
+     */
+    public Point getIntersection(Segment sg) {
+        LineABC line = new LineABC(p, v);
+        Point point = sg.getIntersection(line);
+        if (!sg.contains(point) || !contains(point)) {
+            point = null;
+        }
+        return point;
+    }
+
+    /**
+     * distance from Ray begins to intersection Point
+     *
+     * @param sg segment
+     * @return distance or -1 if intersection doesn't esist
+     */
+    public double distToIntersection(Segment sg) {
+        Point point = getIntersection(sg);
+        if (point == null) {
+            return -1;
+        }
+        return p.distance(point);
+    }
+
+    public boolean isIntersects(Segment sg) {
+        Point point = getIntersection(sg);
+        return point != null;
+    }
 }
