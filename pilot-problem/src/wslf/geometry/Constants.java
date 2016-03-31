c84734d0a6fe995d9f00aeed0be3a031d44defef
Wsl_F <WasylFranchuk@gmail.com>
2016-03-31 17:01:10 +0300

    make all fields public

--- pilot-problem/src/wslf/geometry/Constants.java
+++ pilot-problem/src/wslf/geometry/Constants.java
@@ -4,22 +4,22 @@ package wslf.geometry;
  *
  * @author Wsl_F
  */
-class Constants {
+public class Constants {
 
     /**
      * epsilon value for distances
      */
-    static final double EPS = 1e-8;
+    public static final double EPS = 1e-8;
     /**
      * epsilon angle in radians
      */
-    static final double EPS_ANGLE = 1e-3;
+    public static final double EPS_ANGLE = 1e-3;
     /**
      * Prime number for hash code
      */
-    static final long BIG_PRIME= 5915587277L;
+    public static final long BIG_PRIME= 5915587277L;
     /** 
      * Prime number for hash code
      */
-    static final long LOW_PRIME= 97;
+    public static final long LOW_PRIME= 97;
 }
