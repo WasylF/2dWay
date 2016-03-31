8124bc26d6278044d4031aa5793f052207d68650
Wsl_F <WasylFranchuk@gmail.com>
2016-03-31 17:25:56 +0300

    create Tests for getAngle

---
+++ pilot-problem/test/wslf/geometry/RayTest.java
@@ -0,0 +1,62 @@
+package wslf.geometry;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Wsl_F
+ */
+public class RayTest {
+
+    public RayTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+
+    /**
+     * Test of getAngle method, of class Ray.
+     */
+    @Test
+    public void testGetAngle() {
+        System.out.println("getAngle");
+        Ray instance = new Ray(new Point(0, 0), new Vector(0, 1));
+        Point point = new Point(1, 1);
+        double expResult = -java.lang.Math.PI / 4;
+        double result = instance.getAngle(point);
+        assertEquals(expResult, result, Constants.EPS_ANGLE);
+    }
+
+    /**
+     * Test of getAngle360 method, of class Ray.
+     */
+    @Test
+    public void testGetAngle360() {
+        System.out.println("getAngle360");
+        Ray instance = new Ray(new Point(0, 0), new Vector(0, 1));
+        Point point = new Point(1, 1);
+        double expResult = 7 * java.lang.Math.PI / 4;
+        double result = instance.getAngle360(point);
+        assertEquals(expResult, result, Constants.EPS_ANGLE);
+    }
+
+}
