7b90b6501b143b8d5873ae62650c9abf42c35af2
Wsl_F <WasylFranchuk@gmail.com>
2016-04-22 01:52:01 +0300

    execute build vis gr.

--- pilot-problem/src/wslf/Main.java
+++ pilot-problem/src/wslf/Main.java
@@ -1,5 +1,8 @@
 package wslf;
 
+import wslf.algo.Visibility;
+import wslf.algo.World;
+
 /**
  *
  * @author Wsl_F
@@ -10,7 +13,16 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         // TODO code application logic here
+        World world = World.generateWorld(500);
+        World.printWorld("W.txt", world);
+    //    World world2 = World.readWorld("W.txt");
+
+        Visibility visibility = new Visibility(world);
+        visibility.buildVisibilityGraph();
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("Successfully finished in: "+estimatedTime+" milliseconds");
     }
-    
+
 }
