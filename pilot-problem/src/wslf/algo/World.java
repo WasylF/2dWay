21c6c3020484f3a65d8cac07c7b0c46d30af4eb1
Wsl_F <WasylFranchuk@gmail.com>
2016-04-22 01:32:36 +0300

    add read/write world + very simple world generator

--- pilot-problem/src/wslf/algo/World.java
+++ pilot-problem/src/wslf/algo/World.java
@@ -1,6 +1,14 @@
 package wslf.algo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import wslf.geometry.*;
 
 /**
@@ -70,6 +78,77 @@ public class World {
         }
 
         return barriers[i];
+    }
+
+    public static World readWorld(String fileName) {
+        try {
+            Scanner sc = new Scanner(new File(fileName)).useLocale(Locale.US);
+            int numberOfBarriers = sc.nextInt();
+            Polygon[] barriers = new Polygon[numberOfBarriers];
+
+            for (int i = 0; i < numberOfBarriers; i++) {
+                int numberOfVertices = sc.nextInt();
+                Point[] vertices = new Point[numberOfVertices];
+
+                for (int j = 0; j < numberOfVertices; j++) {
+                    double x = sc.nextDouble();
+                    double y = sc.nextDouble();
+                    vertices[j] = new Point(x, y);
+                }
+
+                barriers[i] = new Polygon(vertices);
+            }
+            return new World(barriers);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(World.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+
+    public static World generateWorld(int numberOfBarriers) {
+        final int blockSizeX = 16;
+        final int blockSizeY = 16;
+
+        Polygon[] barriers = new Polygon[numberOfBarriers];
+
+        for (int i = 0; i < numberOfBarriers; i++) {
+            Point[] vertices = generateTriangle(blockSizeX, blockSizeY);
+
+            for (Point vertex : vertices) {
+                vertex.setX(((double) blockSizeX) * i + vertex.getX());
+            }
+            barriers[i] = new Polygon(vertices);
+
+        }
+        return new World(barriers);
+    }
+
+    private static Point[] generateTriangle(int blockSizeX, int blockSizeY) {
+        Random random = new Random();
+        Point[] vertices = new Point[3];
+        vertices[0] = new Point(1, 1);
+        vertices[1] = new Point(blockSizeX - 2, 1);
+        vertices[2] = new Point(blockSizeX / 2, blockSizeY - 2);
+
+        return vertices;
+    }
+
+    public static void printWorld(String fileName, World world) {
+        try {
+            PrintWriter pw = new PrintWriter(new File(fileName));
+            pw.println(world.barriers.length);
+            for (Polygon barrier : world.barriers) {
+                pw.println(barrier.getSize());
+                for (Point vertex : barrier.getPointsList()) {
+                    pw.print(vertex.getX() + " " + vertex.getY() + " ");
+                }
+                pw.println();
+            }
+            pw.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(World.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
