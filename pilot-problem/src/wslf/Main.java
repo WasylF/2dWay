dff8b14e33824937e7261f85f9d00a9107ec67e8
Wsl_F <WasylFranchuk@gmail.com>
2016-04-22 09:48:40 +0300

    add print to file

--- pilot-problem/src/wslf/Main.java
+++ pilot-problem/src/wslf/Main.java
@@ -1,5 +1,11 @@
 package wslf;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import wslf.algo.Visibility;
 import wslf.algo.World;
 
@@ -13,16 +19,40 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //generate(500);
         long startTime = System.currentTimeMillis();
-        // TODO code application logic here
-        World world = World.generateWorld(500);
-        World.printWorld("W.txt", world);
-    //    World world2 = World.readWorld("W.txt");
+        World world = World.readWorld("W_200.txt");
 
         Visibility visibility = new Visibility(world);
-        visibility.buildVisibilityGraph();
-        long estimatedTime = System.currentTimeMillis() - startTime;
-        System.out.println("Successfully finished in: "+estimatedTime+" milliseconds");
+        ArrayList<ArrayList<Integer>> graph
+                = visibility.buildVisibilityGraph();
+        print("output.txt", graph);
+        double estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("Successfully finished in: " + estimatedTime / 1000 + " seconds");
     }
 
+    public static void generate(int n) {
+        World world_ = World.generateWorld(n);
+        World.printWorld("W_" + n + ".txt", world_);
+
+    }
+
+    public static void print(String fileName, ArrayList<ArrayList<Integer>> visGraph) {
+        try {
+            PrintWriter pw = new PrintWriter(new File(fileName));
+            int size = visGraph.size();
+            pw.println(size);
+            for (int i = 0; i < size; i++) {
+                pw.println(visGraph.get(i).size());
+                for (int v : visGraph.get(i)) {
+                    pw.print(v + " ");
+                }
+                pw.println();
+            }
+            pw.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 }
