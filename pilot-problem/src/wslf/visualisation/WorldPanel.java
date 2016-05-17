658e9c3ac40664214178f259f08d0a6d83d50f41
Wsl_F <WasylFranchuk@gmail.com>
2016-05-17 13:58:59 +0300

    Create form and panel

---
+++ pilot-problem/src/wslf/visualisation/WorldPanel.java
@@ -0,0 +1,20 @@
+package wslf.visualisation;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Wsl_F
+ */
+public class WorldPanel extends JPanel {
+
+    public WorldPanel() {
+        super();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+}
