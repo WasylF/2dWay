3862f79ee17b39add3127b7615f416944a79c1d5
Wsl_F <WasylFranchuk@gmail.com>
2016-05-17 23:51:36 +0300

    add new generation of World + new constructor

--- pilot-problem/src/wslf/algo/World.java
+++ pilot-problem/src/wslf/algo/World.java
@@ -3,7 +3,11 @@ package wslf.algo;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import static java.lang.Double.min;
+import static java.lang.Math.PI;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
@@ -28,6 +32,12 @@ public class World {
 
     public World(Polygon[] barriers) {
         this.barriers = barriers;
+    }
+
+    public World(List<Polygon> barriers) {
+        Polygon[] polygons = new Polygon[barriers.size()];
+        polygons = barriers.toArray(polygons);
+        this.barriers = polygons;
     }
 
     /**
@@ -155,4 +165,70 @@ public class World {
         }
     }
 
+    public static World generateWorld(int blockNumX, int blockNumY, int minX, int maxX, int minY, int maxY) {
+        LinkedList<Polygon> barriers = new LinkedList<>();
+
+        int maxBlockX = maxX - minX;
+        maxBlockX /= blockNumX;
+        int maxBlockY = maxY - minY;
+        maxBlockY /= blockNumY;
+
+        for (int x = 0; x < blockNumX; x++) {
+            for (int y = 0; y < blockNumY; y++) {
+                if (y % 2 == 0) {
+                    barriers.add(generatePolygon(maxBlockX, maxBlockY, maxBlockX * x, maxBlockY * y));
+                } else {
+                    if (y == 0) {
+                        continue;
+                    }
+                    barriers.add(generatePolygon(maxBlockX, maxBlockY, (int) (maxBlockX * (x - 0.55)), (int) (maxBlockY * (y))));
+                }
+            }
+        }
+
+        return new World(barriers);
+    }
+
+    private static Polygon generatePolygon(int maxX, int maxY, int marginX, int marginY) {
+        double centerX = marginX + maxX / 2;
+        double centerY = marginY + maxY / 2;
+
+        final int anglesNum = 30;
+        boolean[] angles = new boolean[anglesNum];
+        angles[0] = true;
+        final int[] numberOfVertices = {3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 7, 8, 9, 10};
+        Random random = new Random();
+
+        int n = random.nextInt(numberOfVertices.length);
+        n = numberOfVertices[n];
+
+        for (int i = 1; i < n; i++) {
+            int angle;
+            do {
+                angle = random.nextInt(anglesNum - 1) + 1;
+            } while (angles[angle]);
+            angles[angle] = true;
+        }
+
+        Point[] vertices = new Point[n];
+        int radius = maxX < maxY ? maxX / 2 : maxY / 2;
+        int maxRand = radius * 10;
+        int index = 0;
+        for (int angle = anglesNum - 1; angle >= 0; angle--) {
+            if (angles[angle]) {
+                Vector v = new Vector(2.0 * angle * PI / anglesNum);
+                v.normalize();
+                double leng = maxRand / 20 + (double) random.nextInt(maxRand) / 20.0;
+                v.multiplyScalar(leng * 1.4);
+                vertices[index++] = new Point(centerX + v.getX(), centerY + v.getY());
+            }
+        }
+
+        Polygon polygon = new Polygon(vertices);
+        if (polygon.area() < (maxX * maxY) / (min(1.2 * n, 5))) {
+            polygon = generatePolygon(maxX, maxY, marginX, marginY);
+        }
+        
+        return polygon;
+    }
 }
